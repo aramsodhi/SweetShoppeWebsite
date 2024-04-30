68531008d365f95edcfddde829fe929093e8d377
Aram Sodhi <93051510+aramsodhi@users.noreply.github.com>
2024-04-30 14:14:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const flavor_container = document.querySelector(".flavor-container");
 
-console.log(flavor_container.childNodes);
-
 flavor_container.childNodes.forEach((flavor, index) => {
     if (index % 2 == 1) {
         flavor.style = `rotate: ${Math.floor(Math.random() * 10 - 5)}deg`;
